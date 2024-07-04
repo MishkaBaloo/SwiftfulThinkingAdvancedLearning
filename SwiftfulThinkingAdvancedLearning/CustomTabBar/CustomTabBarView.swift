2701b058fc662ccc7c72bc073505c4652a3957fb
Mykhailo Tokmakov <misha.tokmak@gmail.com>
2024-07-04 19:08:42 +0300

    Custom TabBar Bootcamp

---
+++ SwiftfulThinkingAdvancedLearning/CustomTabBar/CustomTabBarView.swift
@@ -0,0 +1,115 @@
+//
+//  CustomTabBarView.swift
+//  SwiftfulThinkingAdvancedLearning
+//
+//  Created by Michael on 7/4/24.
+//
+
+import SwiftUI
+
+struct CustomTabBarView: View {
+    
+    let tabs: [TabBarItem]
+    @Binding var selection: TabBarItem
+    @Namespace private var namespace
+    @State var localSelection: TabBarItem
+    
+    var body: some View {
+//        TabBarVersionOne
+        TabBarVersionTwo
+            .onChange(of: selection) { oldValue, newValue in
+                withAnimation(.easeInOut) {
+                    localSelection = newValue
+                }
+            }
+    }
+}
+
+struct CustomTabBarView_Previews: PreviewProvider {
+    
+    static let tabs: [TabBarItem] = [.home, .favorites, .profile]
+    
+    static var previews: some View {
+        VStack {
+            Spacer()
+            CustomTabBarView(tabs: tabs, selection: .constant(tabs.first!), localSelection: tabs.first!)
+        }
+    }
+}
+
+extension CustomTabBarView {
+    
+    private func tabView(tab: TabBarItem) -> some View {
+        VStack {
+            Image(systemName: tab.iconName)
+                .font(.subheadline)
+            Text(tab.title)
+                .font(.system(size: 10, weight: .semibold, design: .rounded))
+        }
+        .foregroundStyle(localSelection == tab ? tab.color : Color.gray)
+        .padding(.vertical, 8)
+        .frame(maxWidth: .infinity)
+        .background(selection == tab ? tab.color.opacity(0.2) : Color.clear)
+        .clipShape(.rect(cornerRadius: 10))
+    }
+    
+    private var TabBarVersionOne: some View {
+        HStack {
+            ForEach(tabs,id: \.self) { tab in
+                tabView(tab: tab)
+                    .onTapGesture {
+                        switchToTab(tab: tab)
+                    }
+            }
+        }
+        .padding(6)
+        .background(Color.white.ignoresSafeArea(edges: .bottom))
+    }
+    
+    private func switchToTab(tab: TabBarItem) {
+        selection = tab
+    }
+    
+}
+
+extension CustomTabBarView {
+    
+    private func tabViewTwo(tab: TabBarItem) -> some View {
+        VStack {
+            Image(systemName: tab.iconName)
+                .font(.subheadline)
+            Text(tab.title)
+                .font(.system(size: 10, weight: .semibold, design: .rounded))
+        }
+        .foregroundStyle(localSelection == tab ? tab.color : Color.gray)
+        .padding(.vertical, 8)
+        .frame(maxWidth: .infinity)
+        .background(
+            ZStack {
+                if localSelection == tab {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(tab.color.opacity(0.2))
+                        .matchedGeometryEffect(id: "bacbground_rectangle", in: namespace)
+                }
+            }
+        )
+    }
+    
+    private var TabBarVersionTwo: some View {
+        HStack {
+            ForEach(tabs,id: \.self) { tab in
+                tabViewTwo(tab: tab)
+                    .onTapGesture {
+                        switchToTab(tab: tab)
+                    }
+            }
+        }
+        .padding(6)
+        .background(Color.white.ignoresSafeArea(edges: .bottom))
+        .clipShape(.rect(cornerRadius: 10))
+        .shadow(color: .black.opacity(0.3), radius: 10, x: 0, y: 5)
+        .padding(.horizontal)
+    }
+    
+}
+
